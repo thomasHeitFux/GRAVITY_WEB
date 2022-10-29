b90bb2e3089ae5bc6c0a501124c06eb7734e71e7
thomasHeitFux <thomas41392@gmail.com>
2022-10-28 22:57:19 -0300

    aguante tailwind

--- src/components/Contact/Contact/Contact.jsx
+++ src/components/Contact/Contact/Contact.jsx
@@ -4,23 +4,26 @@ import imgContact from "../../../assets/imgContact"
 
 function SectionContact() {
     return (
-        <div className="sectionContact container">
-            <div className="cardContact">
-                <div className="circle">
-                    <img src={imgContact.envelopColor} className="develop" />
+        <div className="flex flex-col-reverse pt-12 pb-12 w-full text-center justify-center items-center xl:flex-row  lg:flex-row md:flex-col sm:flex-col ">
+            <div className="cardContact flex w-4/4  p-6 space-x-2 rounded-3xl xl:flex-col lg:flex-col lg:space-x-0 md:flex-row  lg:w-1/4 md:w-4/5 justify-center items-center sm:w-4/5 sm:space-x-16">
+                <div className="flex items-center justify-center h-24 w-24 bg-white rounded-full z-10">
+                    <img src={imgContact.envelopColor} className="w-16" />
                 </div>
+                <div className="flex flex-col justify-center items-center">
                 <h1 className="bold">Te escuchamos</h1>
                 <p> Atención@Gravity.com</p>
-                <button className="btn bold">Enviar Email</button>
+                <button className="btn bold lg:w-full sm:w-48 rounded-xl p-1 ">Enviar Email</button>
+                </div>
             </div>
-            <div className="networkContact">
+            <div className="networkContact flex flex-col items-center pb-5 gap-5  xl:flex-col lg:flex-col lg:w-2/5 md:flex-row sm:w-4/5 sm:pt-5 sm:flex-row">
                 <h1 className="bold">
                     Haz despegar tu marca
                 </h1>
+                <div className="flex flex-col lg:flex-col sm:flex-col-reverse gap-4">
                 <p>
                     Te esperamos en nuestras distintas redes sociales:
                 </p>
-                <ul className="networksContain">
+                <ul className="networksContain flex w-full justify-center gap-3">
                     <li className="networkIcon">
                         <a href="https://www.linkedin.com/company/gravity-agencia-de-marketing-y-publicidad/?viewAsMember=true/" target="_blank">
                             <img src={images.Linkdln_icon} alt="Linkdln" />
@@ -57,6 +60,7 @@ function SectionContact() {
                         </a>
                     </li>
                 </ul>
+                </div>
             </div>
         </div>
     );
