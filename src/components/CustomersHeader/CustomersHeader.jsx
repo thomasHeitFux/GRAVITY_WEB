06af3689d3e40998069c7ad02045eba87ef809d7
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-04 13:12:57 -0300

    responsive en proceso

--- src/components/CustomersHeader/CustomersHeader.jsx
+++ src/components/CustomersHeader/CustomersHeader.jsx
@@ -2,16 +2,18 @@ import "./customersHeader.css";
 
 function CustomersHeader({ banner, name, description }) {
   return (
-    <div className="customers__header__card shadow-card">
+    <div className="shadow-card top-0 right-0">
+
         <img src={banner} className="relative h-80 object-cover w-full lg:h-5/6 lg:w-screen" />
-        <div className="absolute rounded-3xl right-0 lg:w-80 lg:right-40 lg:top-60 bg-white lg:p-10 ">
-          <span className="w-10 rounded-full right-20 bg-black lg:w-20"></span>
+
+        <div className="absolute rounded-3xl w-20 lg:w-80 lg:right-40 lg:top-60 bg-white lg:p-10 ">
+          <span className="w-10 absolute aspect-square -top-10 rounded-full lg:right-32 bg-black lg:w-20"></span>
           <p className="lg:mt-5 text-azul lg:text-base">
             {description}
           </p>
         </div>
+        
       </div>
-
   );
 }
 
