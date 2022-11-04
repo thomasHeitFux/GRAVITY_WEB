b642f6ce92f95f52956916071777738a0f020736
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-04 12:54:07 -0300

    primer responsive clientes

--- src/components/CustomersHeader/CustomersHeader.jsx
+++ src/components/CustomersHeader/CustomersHeader.jsx
@@ -3,10 +3,10 @@ import "./customersHeader.css";
 function CustomersHeader({ banner, name, description }) {
   return (
     <div className="customers__header__card shadow-card">
-        <img src={banner} className="w-screen relative" />
-        <div className="absolute w-80 xl:right-40 top-60 bg-white p-10 rounded-3xl">
-          <span></span>
-          <p className="mt-5 text-azul text-base">
+        <img src={banner} className="relative h-80 object-cover w-full lg:h-5/6 lg:w-screen" />
+        <div className="absolute rounded-3xl right-0 lg:w-80 lg:right-40 lg:top-60 bg-white lg:p-10 ">
+          <span className="w-10 rounded-full right-20 bg-black lg:w-20"></span>
+          <p className="lg:mt-5 text-azul lg:text-base">
             {description}
           </p>
         </div>
