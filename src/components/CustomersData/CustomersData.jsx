49cd6f2f42330dba8a2d10128ba755c65da74499
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-06 19:26:04 -0300

    correcciones responsive

--- src/components/CustomersData/CustomersData.jsx
+++ src/components/CustomersData/CustomersData.jsx
@@ -3,8 +3,9 @@ import "./customersData.css";
 function CustomersData({name, description}) {
   return (
     <div className="ml-10 my-10 lg:ml-20">
+        <h5 className="absolute text-white text-lg font-bold md:hidden -mt-20 -mb-40"> {description.s}</h5>
         <h4 className="text-2xl lg:text-5xl text-azul lg:mb-5 font-bold">{name}</h4>
-        <p className="lg:text-2xl text-azul lg:mb-16">
+        <p className="hidden md:block lg:text-2xl text-azul lg:mb-16">
          {description.s}
         </p>
     </div>
