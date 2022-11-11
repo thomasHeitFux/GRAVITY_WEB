e2eeaf625934e766288e955bb72dc9a5808b9afe
Leandro Pereyra <me.leandrop@gmail.com>
2022-11-11 10:16:42 -0300

    projects home

--- src/components/ProjectsHome/ProjectsHome.jsx
+++ src/components/ProjectsHome/ProjectsHome.jsx
@@ -1,11 +1,14 @@
 import './projectsHome.css'
 import images from '../../assets/imagenes'
 
-function ProjectsHome() {
+function ProjectsHome({projects}) {
     return (
         <div id="Projects">
             <h3 className="title__section">Nuestros proyectos</h3>
             <ul className='business__logo__container flex__center'>
+                {/* {projects.map(p=> {
+                    return <li><img src={p.img} alt={p.name}/></li>
+                })} */}
                 <li><img src={images.Glovo_icon____gravity} alt="Gravity Logo" /></li>
                 <li><img src={images.Glovo_icon____gravity} alt="Gravity Logo" /></li>
                 <li><img src={images.Glovo_icon____gravity} alt="Gravity Logo" /></li>
